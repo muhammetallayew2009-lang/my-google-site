80ec168241d488d26fa1f80c0b982c210cb20cec
muhammetallayew2009-lang <muhammetallayew2009@gmail.com>
2026-03-10 04:49:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("This website is just for learning purposes, so don't expect much :)");
+const welcome = document.getElementById("welcome");
+let aaa;
+aaa = window.prompt("your name:");
+
+welcome.innerText = aaa;
